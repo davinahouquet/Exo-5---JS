32a3b34b2a4c5c2bd45e112e447f1ca13e46281c
davinahouquet <davina.houquet@hotmail.fr>
2023-04-21 10:00:44 +0200

    fonction calcDevise

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
-const resultat = document.getElementById("resultat");
-var nb = document.getElementById("input").value;
+//resultatBalise reprrésente l'affchage du résultat. On ne peut pas mettre juste résultat, qui sera égal à un nombre dans la fonction calcDevise, sinon elle sera écrasée
+var resultatBalise = document.getElementById("resultat"); 
+console.log(resultat);
 
 //Fonction qui calcul (valeur en euros / 6,55957 )
-
 function calcDevise(){
-
-  if(!empty(nb)){
-    resultat = nb/6,55957;
-    resultat.toFixed(2);
+//Variable nb déclarée DANS la fonction pour que la valeur entrée dans l'input soit pris en compte
+var nb = document.getElementById("input").value;
+//Calcul et arrondissement à la deuxième décimale
+  if(nb != null){
+    resultat = nb/6.55957;
+    resultat = resultat.toFixed(2);
+    console.log(resultat);
    }
-   console.log(resultat);
-   "= " +resultat +" francs"
+//Affichage HTML dans la variable crée précedemment
+   resultatBalise.innerHTML = "= " +resultat +" francs"
 }
-
-//Quand on entre une valeur dans input on appelle la fonction calcDevise
-document.getElementById("input").addEventListener("keypres")
